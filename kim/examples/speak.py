37f8dd2729ee6db54fffd8d10a01d267df2fba5f
Morten Amundsen <mortea15@student.uia.no>
2017-08-31 06:21:07 +0200

    Updated the example speak to match the new beep() method

--- kim/examples/speak.py
+++ kim/examples/speak.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8-*-
-import kim
+from kim import kim
 
 spk = kim.Speak(device='plughw:0,0',
-                        resources='../resources/audio')
+                resources='../resources/audio')
 
-spk.beep(1)
-spk.beep(0)
+spk.beep()
 spk.say("Hi, I'm Kim. How are you today, Sir?")
